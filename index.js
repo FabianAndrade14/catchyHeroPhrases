cbf6db84b8aa2958f512cadd18faaa99e32044bc
Fabian Andrade <40735444+FabianAndrade14@users.noreply.github.com>
2025-07-01 22:24:36 -0500

    fix(main): Adding some quotes for testing

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+const quotes = require('./quotes.json');
+
+const app = express();
+const PORT = 3000;
+
+// Swagger doc
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+//Endpoint
+app.get('/', (req, res) => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    res.json(quotes[randomIndex]);
+});
+
+// Id
+app.get('/api/quote/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const quote = quotes.find(q => q.id === id);
+    if (quote) {
+        res.json(quote);
+    } else {
+        res.status(404).json({ error: 'Frase no encontrada'})
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
